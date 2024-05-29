c375e3489932aae8527b009866c5e28613a8c0b8
Bryan Wilhite <bryan.wilhite@gmail.com>
2024-05-28 17:42:13 -0700

    🚜✨ ⚙🖋 same chess game with player commentary

--- chess/songhay/utilities/ipython.py
+++ chess/songhay/utilities/ipython.py
@@ -2,14 +2,20 @@ from IPython import display
 import chess
 import chess.svg
 
-def display_svg(svg):
-    svg = ''.join([
+def display_html_from_list(htmlList):
+    html = list()
+    for htmlFragment in htmlList:
+        html.append(htmlFragment)
+    return display.HTML(''.join(html))
+
+def display_svg(html):
+    html = ''.join([
         '<div id="svg-block" style="display: inline-block">',
-        svg,
+        html,
         '</div>',
     ])
 
-    return svg
+    return html
 
 def display_svgs(svgs):
     html = list()
@@ -22,3 +28,15 @@ def display_taken(taken):
     for piece in taken:
         svgs.append(chess.svg.piece(chess.Piece.from_symbol(piece), size=24))
     return display_svgs(svgs)
+
+def get_board_and_push(board, move, black_taken, white_taken):
+    board.push(move)
+    html = ''.join([
+        '<div id="svg-board-block" style="display: inline-block; margin:.5em">',
+        f'    <div>{display_taken(black_taken)}</div>',
+        chess.svg.board(board, size=240),
+        f'    <div>{display_taken(white_taken)}</div>',
+        '</div>',
+    ])
+
+    return html
