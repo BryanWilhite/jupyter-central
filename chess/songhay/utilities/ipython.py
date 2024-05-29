bc017633f13cfe9788248801ba7fad8eba73418d
Bryan Wilhite <bryan.wilhite@gmail.com>
2024-05-28 22:11:18 -0700

    🚜✨ ⚙🖋 same chess game with player commentary

--- chess/songhay/utilities/ipython.py
+++ chess/songhay/utilities/ipython.py
@@ -17,26 +17,25 @@ def display_svg(html):
 
     return html
 
-def display_svgs(svgs):
-    html = list()
-    for svg in svgs:
-        html.append(display_svg(svg))
-    return display.HTML(''.join(html))
+def display_taken(taken, size=24):
+    return display.HTML(get_taken(taken, size=size))
 
-def display_taken(taken):
-    svgs = list()
-    for piece in taken:
-        svgs.append(chess.svg.piece(chess.Piece.from_symbol(piece), size=24))
-    return display_svgs(svgs)
-
-def get_board_and_push(board, move, black_taken, white_taken):
+def get_board_and_push(board, move, black_taken = [], white_taken = [], size=240, arrows = []):
     board.push(move)
     html = ''.join([
         '<div id="svg-board-block" style="display: inline-block; margin:.5em">',
-        f'    <div>{display_taken(black_taken)}</div>',
-        chess.svg.board(board, size=240),
-        f'    <div>{display_taken(white_taken)}</div>',
+        f'    <h2>{move.uci()}{": check " if board.is_check() else ""}</h2>',
+        f'    <div>{get_taken(white_taken)}</div>',
+        chess.svg.board(board, arrows=arrows, size=size) if len(arrows) > 0 else chess.svg.board(board, size=size),
+        f'    <div>{get_taken(black_taken)}</div>',
         '</div>',
     ])
 
     return html
+
+def get_taken(taken, size=24):
+    svgs = list()
+    for piece in taken:
+        svgs.append(chess.svg.piece(chess.Piece.from_symbol(piece), size=size))
+
+    return f'<div style="background-color:rgba(255, 255, 255, 0.5);margin:.5em">{"".join(svgs)}</div>'
